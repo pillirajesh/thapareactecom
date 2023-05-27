4531d0eb9a7d53758093c43b50b34ec16fb43f4e
pillirajesh <pillirajesh@ymail.com>
2023-05-27 20:16:37 +0530

    code added

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -8,7 +8,10 @@ const Contact = () => {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.755807076232!2d88.34870107480658!3d22.73731597937482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f89b1ea939a2f5%3A0xf04353dc5898ef13!2sALCOVE%20NEW%20KOLKATA%20-%20Home%20by%20the%20Ganges!5e0!3m2!1sen!2sin!4v1685084521087!5m2!1sen!2sin"
         width="90%"
         height="300"
-        style={{ border: "0px", borderRadius: "20px", marginBottom: "15px" }}
+        style={{
+          borderRadius: "20px",
+          marginBottom: "15px",
+        }}
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
--- src/ErrorPage/ErrorPage.js
+++ src/ErrorPage/ErrorPage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./ErrorPage.css";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="error-container">
+      <h1>404</h1>
+      <h5>UH OH! You're lost.</h5>
+      <p>
+        This page you are looking for does't exist. You can click the button
+        below to go back to the homepage.
+      </p>
+      <Link to="/" className="link">
+        <button className="button">Home</button>
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
